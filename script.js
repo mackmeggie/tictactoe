8953909f49e61e242db938ad5103002de87f4e18
mackmeggie <mack.meggie13@gmail.com>
2022-03-30 16:54:07 -0600

    create gameboard function

---
+++ script.js
@@ -0,0 +1,15 @@
+
+//Gameboard stored as array inside factory function
+const gameboard = (() => {
+
+    const board = [{
+    a:1, a:2, a:3,
+    b:1, b:2, b:3,
+    c:1, c:2, c:3
+    }];
+
+    const displayboard = () => {
+        
+    }
+
+}
