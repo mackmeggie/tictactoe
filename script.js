4aa6fc70eaf154df558239ba387251bd344037a6
mackmeggie <mack.meggie13@gmail.com>
2022-04-03 21:59:58 -0600

    create gameboard

--- script.js
+++ script.js
@@ -1,71 +1,49 @@
 
-//Gameboard stored as array inside module
+//Gameboard module
 const gameboard = (() => {
-
     const board = [
-    {a:1}, {a:2}, {a:3},
-    {b:1}, {b:2}, {b:3},
-    {c:1}, {c:2}, {c:3}
+        "X", "O", "X",
+        "O", "X", "O",
+        "X", "O", "X",
     ];
 
+    
 
-//function creates gameboard
+    //display gameboard function
     const displayboard = () => {
         
-        board.forEach((object, index) => {
-        let gamegrid = document.querySelector('.gameboard');
-           let cell = document.createElement('div');
-           cell.classList.add('gamecell');
-           cell.setAttribute('id',`${index}`);
-           cell.textContent=""
-           gamegrid.appendChild(cell);
-           gamegrid.style.gridTemplateRows=`repeat(3, 1fr)`
-           gamegrid.style.gridTemplateColumns=`repeat(3, 1fr)` 
-           cell.addEventListener('click', () => {
-               cell.textContent="O";
-            });
+        board.forEach((item, string, index) => {
+            let gamegrid = document.querySelector('.gameboard');
+            let cell = document.createElement('div');
+            cell.classList.add('gamecell');
+            cell.textContent=board[string];
+            gamegrid.appendChild(cell);
+            gamegrid.style.gridTemplateRows=`repeat(3, 1fr)`
+            gamegrid.style.gridTemplateColumns=`repeat(3, 1fr)` 
         })
     }
 
+    
     return {
         board, displayboard,
     };
 })();
 
-
-
 //Player factory function
 const player = (name, marker) => {
-    name = prompt("Please enter your name");
-    marker = prompt("Please select a marker");
 
-    const playMarker = marker => {
-        if (textContent == '') {
-            textContent = marker;
-        } if (textContent != marker) {
-            textContent = marker;
-        } 
-    }
+    const playMarker = () => 
 
     return {
-        name, marker, playMarker, 
+        name, marker,
     }
 }
 
-const gameflow = () => {
+//Gameflow module
+const gameplay = (() => {
 
-    
-}
-
-const player1 = player();
-const player2 = player()
-
-
-
+})();
 
 gameboard.displayboard();
-
-
-
 
 
