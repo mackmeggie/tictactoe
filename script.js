52a0f61f86d21923b0b2c08cfcb9b38999747d01
mackmeggie <mack.meggie13@gmail.com>
2022-04-09 13:19:28 -0600

    playX and playO functions added - not completely functional

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 //Gameboard module
 const gameboard = (() => {
     const board = [
-        "X", "O", "X",
-        "O", "X", "O",
-        "X", "O", "X",
+        "*", "*", "*",
+        "*", "*", "*",
+        "*", "*", "*",
     ];
 
     
@@ -12,27 +12,62 @@ const gameboard = (() => {
     //display gameboard function
     const displayboard = () => {
         
-        board.forEach((item, string, index) => {
+        board.forEach((item, index) => {
             let gamegrid = document.querySelector('.gameboard');
             let cell = document.createElement('div');
             cell.classList.add('gamecell');
-            cell.textContent=board[string];
+            cell.setAttribute('id', `${index}`)
+            cell.textContent="*"
             gamegrid.appendChild(cell);
             gamegrid.style.gridTemplateRows=`repeat(3, 1fr)`
             gamegrid.style.gridTemplateColumns=`repeat(3, 1fr)` 
+            cell.addEventListener('click', playO(`${index}`));
+            cell.addEventListener('click', playX(`${index}`))
         })
     }
 
+    const test = () => {
+        console.log('Helooooo, is anyting working???')
+    }
     
+    const playX = (indexy) => {
+        const cell = document.getElementById(`${indexy}`);
+        if(cell.textContent === "*") {
+            cell.textContent = "X"
+        } 
+        if (cell.textContent === "O" || cell.textContent === "X") {
+            alert("Spot already selected.")
+        }
+    }
+
+
+    const playO = (indexy) => {
+        const cell = document.getElementById(`${indexy}`)
+        if(cell.textContent === "*") {
+            cell.textContent = "O"
+            console.log("???")
+        }
+        if(cell.textContent === "O" || cell.textContent === "X") {
+            alert("Spot already selected.")
+            console.log("mmhmmm")
+        } 
+}
+    
+
     return {
-        board, displayboard,
+        board, displayboard, playO, playX, test
     };
 })();
+
+
+
+
 
 //Player factory function
 const player = (name, marker) => {
 
-    const playMarker = () => 
+    
+    
 
     return {
         name, marker,
