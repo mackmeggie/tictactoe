1d1e98d5e3192dde36e97455b5519c8fb4066845
mackmeggie <mack.meggie13@gmail.com>
2022-04-13 18:11:09 -0600

    playmarker functions edit

--- script.js
+++ script.js
@@ -21,40 +21,14 @@ const gameboard = (() => {
             gamegrid.appendChild(cell);
             gamegrid.style.gridTemplateRows=`repeat(3, 1fr)`
             gamegrid.style.gridTemplateColumns=`repeat(3, 1fr)` 
-            cell.addEventListener('click', playO);
-            cell.addEventListener('click', playX)
+           // cell.addEventListener('click', playMarkerO)
         })
     }
 
-    const test = () => {
-        console.log('Helooooo, is anyting working???')
-    }
-    
-    const playX = (event) => {
-        const cell = event.target;
-        if(cell.textContent === "*") {
-            cell.textContent = "X"
-        } 
-        if (cell.textContent === "O" || cell.textContent === "X") {
-            alert("Spot already selected.")
-        }
-    }
-
-
-    const playO = (event) => {
-        const cell = event.target
-        if(cell.textContent === "*") {
-            cell.textContent = "O"
-        }
-        if(cell.textContent === "O" || cell.textContent === "X") {
-            alert("Spot already selected.")
-            console.log("mmhmmm")
-        } 
-}
     
 
     return {
-        board, displayboard, playX, playO, test
+        board, displayboard, //playMarkerO
     };
 })();
 
@@ -65,19 +39,69 @@ const gameboard = (() => {
 //Player factory function
 const player = (name, marker) => {
 
+   // name = prompt("Name please:");
+   // marker = prompt("X or O?")
+
+const playMarkerO = (event, marker) => {
+        
+        const cell = event.target
+        if(cell.textContent === "*") {
+            cell.textContent = `${player1.marker}`
+        }
+        if(cell.textContent === "O" || cell.textContent === "X") {
+            alert("Spot already selected.")
+        } }
+
+const playMarkerX = (event, marker) => {
+        
+        const cell = event.target
+        if(cell.textContent === "*") {
+                cell.textContent = `${player2.marker}`
+            }
+        if(cell.textContent === "O" || cell.textContent === "X") {
+                alert("Spot already selected.")
+            } }
     
+const applyeventO = () => {
+    gamegrid = document.querySelector('.gameboard');
+    gamegrid.addEventListener('click', playMarkerO);
+}
     
+const applyeventX = () => {
+    gamegrid = document.querySelector('.gameboard');
+    gamegrid.addEventListener('click', playMarkerX);
+}
 
     return {
-        name, marker,
+        name, marker, playMarkerO, playMarkerX, applyeventO, applyeventX
     }
 }
 
 //Gameflow module
 const gameplay = (() => {
 
+    
+
+    const begingame = () => {
+
+    }
+    
+
+    return {
+        
+    };
+
+
 })();
 
 gameboard.displayboard();
+
+const player1 = player("Meeg", "O");
+const player2 = player("Max", "X");
+
+player1.applyeventO()
+player2.applyeventX();
+
+
 
 
