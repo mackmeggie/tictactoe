82f2c2dfefee7cb1eefd7ffb78b08015fed641bd
mackmeggie <mack.meggie13@gmail.com>
2022-04-25 11:08:18 -0600

    playMarker function operational. Commented out code removed.

--- script.js
+++ script.js
@@ -21,87 +21,64 @@ const gameboard = (() => {
             gamegrid.appendChild(cell);
             gamegrid.style.gridTemplateRows=`repeat(3, 1fr)`
             gamegrid.style.gridTemplateColumns=`repeat(3, 1fr)` 
-           // cell.addEventListener('click', playMarkerO)
         })
     }
 
     
 
     return {
-        board, displayboard, //playMarkerO
+        board, displayboard, 
     };
 })();
-
-
-
 
 
 //Player factory function
 const player = (name, marker) => {
 
-   // name = prompt("Name please:");
-   // marker = prompt("X or O?")
-
-const playMarkerO = (event, marker) => {
-        
-        const cell = event.target
-        if(cell.textContent === "*") {
-            cell.textContent = `${player1.marker}`
-        }
-        if(cell.textContent === "O" || cell.textContent === "X") {
-            alert("Spot already selected.")
-        } }
-
-const playMarkerX = (event, marker) => {
-        
-        const cell = event.target
-        if(cell.textContent === "*") {
-                cell.textContent = `${player2.marker}`
-            }
-        if(cell.textContent === "O" || cell.textContent === "X") {
-                alert("Spot already selected.")
-            } }
-    
-const applyeventO = () => {
-    gamegrid = document.querySelector('.gameboard');
-    gamegrid.addEventListener('click', playMarkerO);
-}
-    
-const applyeventX = () => {
-    gamegrid = document.querySelector('.gameboard');
-    gamegrid.addEventListener('click', playMarkerX);
-}
-
     return {
-        name, marker, playMarkerO, playMarkerX, applyeventO, applyeventX
+        name, marker,
     }
 }
 
 //Gameflow module
 const gameplay = (() => {
 
-    
+    const player1 = player('player1', 'O')
+    const player2 = player('player2', 'X')
 
-    const begingame = () => {
+    let currentPlayer = player1
 
+    const switchPlayer = () => {
+        if (currentPlayer === player1) {
+            currentPlayer = player2
+        } else {
+            currentPlayer = player1
+        }
     }
     
+    const playMarker = (event) => {
+        const cell = event.target
+        if(cell.textContent === "*") {
+            cell.textContent = currentPlayer.marker
+        } 
+        if(cell.textContent === "O" || cell.textContent === "X") {
+            alert("Spot already selected.")
+        } }
+
+    const applyevent = () => {
+        gamegrid = document.querySelector('.gameboard');
+        gamegrid.addEventListener('click', playMarker);
+} 
+
 
     return {
-        
+        playMarker, applyevent, switchPlayer, 
     };
 
 
 })();
 
 gameboard.displayboard();
-
-const player1 = player("Meeg", "O");
-const player2 = player("Max", "X");
-
-player1.applyeventO()
-player2.applyeventX();
-
-
+gameplay.applyevent();
 
 
